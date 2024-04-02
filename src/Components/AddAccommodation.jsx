f365549614011f5c5071eff772b852f3aca63d59
arwegasira <ac.rwegasira@gmail.com>
2024-04-02 20:56:25 +0200

    more fields

--- src/Components/AddAccommodation.jsx
+++ src/Components/AddAccommodation.jsx
@@ -1,8 +1,11 @@
 import { Form } from 'react-router-dom'
 import FormInput from './FormInput'
+import FormSelect from './FormSelect'
+import EditDialogTitle from './EditDialogTitle'
 const AddAccommodation = () => {
   return (
     <div>
+      <EditDialogTitle title='Add Accommodation'></EditDialogTitle>
       <Form>
         <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
           <FormInput
@@ -17,8 +20,27 @@ const AddAccommodation = () => {
             name='endDate'
             size='input-sm'
           ></FormInput>
+          <FormSelect
+            label='Choose Room'
+            name='roomName'
+            size='select-sm'
+            defaultSelectTitle='Choose Room'
+          ></FormSelect>
+          <FormInput
+            label='Unit Price'
+            type='number'
+            name='unitPrice'
+            size='input-sm'
+            disabled={true}
+          ></FormInput>
+          <FormInput
+            label='Override Price'
+            type='number'
+            name='price'
+            size='input-sm'
+          ></FormInput>
         </div>
-        <div className='mt-6 flex flex-col gap-3 md:flex-row md:justify-center'>
+        <div className='mt-8 flex flex-col gap-3 md:flex-row md:justify-center'>
           <button type='button' className='btn secondary-btns md:w-[30%]'>
             Cancel
           </button>
