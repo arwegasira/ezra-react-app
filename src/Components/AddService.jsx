c9783006074c42bf0f0185064cc48b67c1b170d0
arwegasira <ac.rwegasira@gmail.com>
2024-04-11 14:21:42 +0200

    add new service functionality

--- src/Components/AddService.jsx
+++ src/Components/AddService.jsx
@@ -1,22 +1,49 @@
 import { useDispatch } from 'react-redux'
-import { Form } from 'react-router-dom'
+import { Form, useLoaderData, useNavigate } from 'react-router-dom'
 import EditDialogTitle from './EditDialogTitle'
 import { closeEditDialog } from '../feature/EditDialog/editDialog'
+import { showAlert } from '../feature/ErrorAlert/ErrorAlert'
 import FormInput from './FormInput'
+import FormSelectMultiPurpose from './FormSelectMultiPurpose'
+import { customFetch } from '../utils'
 const AddService = () => {
+  const {
+    client: { _id: clientId },
+  } = useLoaderData()
   const dispatch = useDispatch()
-  const handleSubmit = (e) => {
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    let formData = new FormData(e.currentTarget)
+    formData = Object.fromEntries(formData)
+    try {
+      const response = await customFetch.post(
+        `/client/addservice/${clientId}`,
+        formData
+      )
+      dispatch(closeEditDialog())
+      navigate(`/singleClient/${clientId}`)
+    } catch (error) {
+      const msg = error?.response?.data.msg || 'Something went wrong'
+      console.error(msg)
+      dispatch(showAlert({ msg: msg }))
+    }
   }
   return (
     <div>
       <EditDialogTitle title='Add Service'></EditDialogTitle>
       <Form method='POST' onSubmit={(e) => handleSubmit(e)}>
-        <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
-          <FormInput
+        <div className='grid grid-cols-1 gap-2 md:grid-cols-2 '>
+          <FormSelectMultiPurpose
             label='Service'
-            type='text'
             name='service'
+            size='select-sm'
+            options={[' ', 'Food and Beverages', 'Laundry']}
+          ></FormSelectMultiPurpose>
+          <FormInput
+            label='Amount'
+            type='number'
+            name='price'
             size='input-sm'
           ></FormInput>
         </div>
