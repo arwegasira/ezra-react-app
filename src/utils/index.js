189b7d3d6460a092178ee1dae27e83807e659a2d
arwegasira <87615127+arwegasira@users.noreply.github.com>
2024-04-18 22:06:19 +0200

    Update index.js
    update base url

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const customFetch = axios.create({
-  baseURL: 'http://localhost:80/api/v1',
+  baseURL: 'https://ezra-back-end.onrender.com/api/v1',
 })
 
 export const dateFormat = (date) => {
