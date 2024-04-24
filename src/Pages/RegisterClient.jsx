42b4e2782f54d55acf37c6e53be3e5a7e13a21de
arwegasira <ac.rwegasira@gmail.com>
2024-04-24 22:49:21 +0200

    reg form

--- src/Pages/RegisterClient.jsx
+++ src/Pages/RegisterClient.jsx
@@ -1,4 +1,10 @@
+import ClientRegForm from '../Components/ClientRegForm'
+
 const RegisterClient = () => {
-  return <div>RegisterClient</div>
+  return (
+    <section className='flex justify-center mt-8'>
+      <ClientRegForm></ClientRegForm>
+    </section>
+  )
 }
 export default RegisterClient
