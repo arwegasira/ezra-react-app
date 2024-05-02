54f0538fcfcea11c4ea2f1bf54ba5f44f8d19d61
arwegasira <ac.rwegasira@gmail.com>
2024-05-02 19:43:41 +0200

    loader

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,9 +1,35 @@
 import UserFilter from '../Components/UserFilter'
+import UsersList from '../Components/UsersList'
+import { customFetch } from '../utils'
 
+export const loader = async ({ request }) => {
+  const url = new URL(request.url)
+  const params = Object.fromEntries(url.searchParams.entries())
+  try {
+    const response = await customFetch.get(
+      `/users?name=${params.name ? params.name : ''}&email=${
+        params.email ? params.email : ''
+      }&role=${params.role ? params.role : ''}`
+    )
+
+    return { users: response?.data?.users, meta: response.data.meta, params }
+  } catch (error) {
+    return error
+  }
+}
 const Users = () => {
   return (
     <div>
+      <div className='mb-8 flex justify-center sm:justify-end w-[90%] max-w-[70rem] mx-auto'>
+        <button
+          type='button'
+          className='btn primary-btns btn-sm w-[90%] max-w-72'
+        >
+          New User
+        </button>
+      </div>
       <UserFilter></UserFilter>
+      <UsersList></UsersList>
     </div>
   )
 }
