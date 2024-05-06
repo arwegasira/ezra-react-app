e9d9295c9c7dfb2d8d028ef9e65261d7574c0622
arwegasira <ac.rwegasira@gmail.com>
2024-05-06 20:27:07 +0200

    loader function fetch rooms

--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -1,8 +1,39 @@
+import { FilterRooms } from '../Components'
+import { customFetch } from '../utils'
+
 export const loader = async ({ request }) => {
-  console.log(request)
-  return null
+  const url = new URL(request.url)
+  const params = Object.fromEntries(url.searchParams.entries())
+  try {
+    const response = await customFetch.get(
+      `/rooms?name=${params.name ? params.name : ''}&roomType=${
+        params.roomType ? params.roomType : ''
+      }&status=${params.status ? params.status : ''}`
+    )
+
+    return {
+      rooms: response?.data?.rooms,
+      params,
+      page: response?.data?.page,
+      pageCount: response?.data?.numOfPages,
+    }
+  } catch (error) {
+    return error
+  }
 }
 const Rooms = () => {
-  return <div>Rooms</div>
+  return (
+    <>
+      <section className='mb-8 flex justify-center sm:justify-end w-[90%] max-w-[70rem] mx-auto'>
+        <button
+          type='button'
+          className='btn primary-btns btn-sm w-[90%] max-w-72'
+        >
+          New User
+        </button>
+      </section>
+      <FilterRooms></FilterRooms>
+    </>
+  )
 }
 export default Rooms
