a76f4a06b1d62a63158910703b78e871d20ee4a6
arwegasira <ac.rwegasira@gmail.com>
2024-05-14 12:37:13 +0200

    rooms table

--- src/Components/RoomsList.jsx
+++ src/Components/RoomsList.jsx
@@ -1,7 +1,58 @@
 import { useLoaderData } from 'react-router-dom'
+import { FaRegEdit } from 'react-icons/fa'
+import { dateFormatShortMonth } from '../utils'
 const RoomsList = () => {
   const { rooms } = useLoaderData()
   console.log(rooms)
-  return <div>RoomsList</div>
+  return (
+    <section className='mt-10 md:flex md:justify-center overflow-x-scroll'>
+      <table className='table table-xs md:table-md w-full md:w-[90%] md:max-w-[80rem]'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Price</th>
+            <th>Status</th>
+            <th>From</th>
+            <th>To</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {rooms.map((room, idx) => {
+            const {
+              name,
+              price,
+              status,
+              roomType,
+              accupationEnd: occupationEnd,
+              accupationStart: occupationStart,
+              _id: roomId,
+            } = room
+
+            return (
+              <tr key={roomId} className='hover'>
+                <td>{name}</td>
+                <td>{roomType}</td>
+                <td>{price}</td>
+                <td>{status}</td>
+                <td>
+                  {occupationStart ? dateFormatShortMonth(occupationStart) : ''}
+                </td>
+                <td>
+                  {occupationEnd ? dateFormatShortMonth(occupationEnd) : ''}
+                </td>
+                <td className='flex justify-end'>
+                  <button type='button' className='text-sm md:text-lg'>
+                    <FaRegEdit></FaRegEdit>
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </section>
+  )
 }
 export default RoomsList
