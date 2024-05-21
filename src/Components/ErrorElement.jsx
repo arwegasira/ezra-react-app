cb3c65f449f79e9827779e65b1ee54d44d464af3
arwegasira <ac.rwegasira@gmail.com>
2024-05-21 20:23:46 +0200

    handle forbidden error

--- src/Components/ErrorElement.jsx
+++ src/Components/ErrorElement.jsx
@@ -2,7 +2,13 @@ import { useRouteError } from 'react-router-dom'
 
 const ErrorElement = () => {
   const error = useRouteError()
-  console.log(error)
+  if (error.status === 403) {
+    return (
+      <div className='grid place-items-center h-[100svh]'>
+        <h4 className='font-bold text-4xl'>Forbidden...</h4>;
+      </div>
+    )
+  }
   return (
     <div className='grid place-items-center h-[100svh]'>
       <h4 className='font-bold text-4xl'>There was an error...</h4>;
