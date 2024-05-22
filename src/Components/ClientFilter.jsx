2ef10adadcb64012eef51634106852d2a32631ec
arwegasira <ac.rwegasira@gmail.com>
2024-05-22 20:38:40 +0200

    clear filter btn

--- src/Components/ClientFilter.jsx
+++ src/Components/ClientFilter.jsx
@@ -1,4 +1,4 @@
-import { Form, useLoaderData } from 'react-router-dom'
+import { Form, useLoaderData, useNavigate } from 'react-router-dom'
 import FormInput from './FormInput'
 import { customFetch } from '../utils'
 import { useSelector } from 'react-redux'
@@ -6,6 +6,7 @@ import { updateClientsFromApi } from '../feature/clients/clientSlice'
 
 const ClientFilter = () => {
   const { params } = useLoaderData()
+  const navigate = useNavigate()
   return (
     <section className='md:flex md:justify-center '>
       <Form className='bg-blue-50 px-4 py-8 rounded-lg shadow-sm w-full grid gap-4 sm:grid-cols-2 md:grid-cols-3 sm:py-6 md:gap-y-1 md:px-10 lg:py-12'>
@@ -24,19 +25,13 @@ const ClientFilter = () => {
           defaultValue={params.phoneNumber && params.phoneNumber}
         ></FormInput>
         <FormInput
-          label='First Name'
+          label='Name'
           type='text'
-          name='firstName'
+          name='name'
           size='input-sm'
           defaultValue={params.firstName && params.firstName}
         ></FormInput>
-        <FormInput
-          label='Last Name'
-          type='text'
-          name='lastName'
-          size='input-sm'
-          defaultValue={params.lastName && params.lastName}
-        ></FormInput>
+
         <FormInput
           label='Email'
           type='email'
@@ -50,6 +45,15 @@ const ClientFilter = () => {
             className='btn primary-btns btn-sm w-full rounded-lg'
           >
             Apply Filters
+          </button>
+        </div>
+        <div className='flex items-end'>
+          <button
+            type='button'
+            className='btn secondary-btns btn-sm w-full rounded-lg'
+            onClick={() => navigate('/home')}
+          >
+            Clear Filters
           </button>
         </div>
       </Form>
